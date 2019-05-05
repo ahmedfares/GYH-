6010d315d93c6e19bbdc458bcc8f6da3fa723978
unknown <ahmed.fares90@gmail.com>
2019-05-05 13:32:25 +0200

    update local bugs

--- src/components/DrawerItem.js
+++ src/components/DrawerItem.js
@@ -13,11 +13,11 @@ export default class DrawerItem extends Component<{}> {
     this.strings = new LocalizedStrings(user.words);
     this.strings.setLanguage(user.lang);
     this.imageUrls = [require('../images/first.png'), require('../images/2-1.png'), require('../images/3-1.png'),
-                      require('../images/4-1.png'), require('../images/5-1.png'), require('../images/6-1.png'),
+                      require('../images/4-1.png'), require('../images/5-1.png'), require('../images/6-1.jpg'),
                       require('../images/7-1.png'), require('../images/8-1.png'), require('../images/10-1.png'),
                       require('../images/11-1.png')];
     this.imageSelectedUrls = [require('../images/1.png'), require('../images/2.png'), require('../images/3.png'),
-                              require('../images/4.png'), require('../images/5.png'), require('../images/6.png'),
+                              require('../images/4.png'), require('../images/5.png'), require('../images/6.jpg'),
                               require('../images/7.png'), require('../images/8.png'), require('../images/10.png'),
                               require('../images/11.png')];
   }
